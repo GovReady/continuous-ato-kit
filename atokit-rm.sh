e6c335fc87b2c329c74626851e226d249f63d893
Peter Kaminski <kaminski@istori.com>
2017-12-11 11:30:58 -0500

    add 'errors okay' echo

--- atokit-rm.sh
+++ atokit-rm.sh
@@ -1,3 +1,4 @@
 #!/bin/sh
 docker-compose rm -s
 docker-compose down -v --rmi all
+echo "\nNOTE: 'not found' errors are okay and to be expected if this script is run after atokit-down.sh, or if this script is run more than once.\n"
